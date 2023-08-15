7214ab3254d6b40e813763bb200df7435f36de82
NatKwok <natkwok902@gmail.com>
2023-08-15 18:16:24 -0400

    Updated README, artisan migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('dob')->nullable(false);
+            $table->char('cell',10)->nullable(false);
             $table->rememberToken();
             $table->timestamps();
         });
